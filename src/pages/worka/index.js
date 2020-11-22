f56a9596f87e4e7f5206609731332ffe0694b2c2
MelanieLinares <mnmclaughlin0@gmail.com>
2020-11-22 10:43:17 -0500

    Revert "removing excess"
    This reverts commit 5b0b18f9c948f5f8736e1905969ad71e895b8301.

--- src/pages/worka/index.js
+++ src/pages/worka/index.js
@@ -7,11 +7,31 @@ export default class ArticleIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div>
-          <h1>
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundImage: `url('/img/blog-index.jpg')`,
+          }}
+        >
+          <h1
+            className="has-text-weight-bold is-size-1"
+            style={{
+              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+              backgroundColor: '#f40',
+              color: 'white',
+              padding: '1rem',
+            }}
+          >
             Latest worka Stories
           </h1>
         </div>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <ArticleRoll />
+            </div>
+          </div>
+        </section>
       </Layout>
     )
   }
