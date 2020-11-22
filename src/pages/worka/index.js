cad3b10f756a73e1cca632cf265552a574eb4ca9
MelanieLinares <mnmclaughlin0@gmail.com>
2020-11-22 11:31:35 -0500

    forcing 404 content

--- src/pages/worka/index.js
+++ src/pages/worka/index.js
@@ -1,18 +1,13 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
-import Layout from '../../components/Layout'
-import ArticleRoll from '../../components/ArticleRoll'
+const NotFoundPage = () => (
+  <Layout>
+    <div>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  </Layout>
+)
 
-export default class ArticleIndexPage extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <div>
-          <h1>
-            Latest worka Stories
-          </h1>
-        </div>        
-      </Layout>
-    )
-  }
-}
+export default NotFoundPage
