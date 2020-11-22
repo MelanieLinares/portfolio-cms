811005553f462c1ab34e986a43ebb46605173e71
MelanieLinares <mnmclaughlin0@gmail.com>
2020-11-22 11:38:44 -0500

    .

---
+++ src/pages/worka/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../../components/Layout'
+import ArticleRoll from '../../components/ArticleRoll'
+
+export default class ArticleIndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+    
+      </Layout>
+    )
+  }
+}
